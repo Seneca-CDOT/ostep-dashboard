a19a7fb21cf153e7d8425be664fe02d9dd9e3050
Pouya Oftadeh <poftadeh2@gmail.com>
2018-09-12 01:34:50 -0400

    Add Github and Lamp Formats

--- Dashboard/src/components/Panel.js
+++ Dashboard/src/components/Panel.js
@@ -1,34 +1,85 @@
 import React from 'react';
-import refresh from './assets/refresh.svg'
+import refresh from './assets/refresh.svg';
+import bulbOn from './assets/bulb-on.svg';
+import bulbOff from './assets/bulb-off.svg';
 
 class Panel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.formatGithub = this.formatGithub.bind(this);
+    this.formatOutput = this.formatOutput.bind(this);
+  }
   formatOutput(panelType) {
-    switch (this.props.title) {
-      case "github": 
-        formatGithub();
+    switch (panelType) {
+      case "github":
+        return this.formatGithub();
         break;
       case "infrastructure":
-        formatInfrastructure();
+        return this.formatInfrastructure();
         break;
       case "ra presentations":
-        formatRAPresentations();
+        return this.formatRAPresentations();
         break;
-      case "slack":
-        formatSlack();
+      case "eods":
+        return this.formatEOD();
         break;
-      case "lamp":
-        formatLamp();
+      case "the lamp":
+        return this.formatLamp();
         break;
       case "room 1042":
-        formatRoom1042();
+        return this.formatRoom1042();
         break;
       default:
         throw new Error(`${panelType} is not a valid panel type!`);
     }
   }
 
+  formatGithub() {
+    return this.props.data.map((commit) => (
+      <li className="github-entry"><span className="github-name">John Kimble</span> committed to  
+         <span className="github-repo"> TSCompare</span>: "Add flexbox to some long repo message that is really way too long to fit."</li>
+    ));
+  }
+
+  formatInfrastructure() {
+
+  }
+
+  formatEOD() {
+
+  }
+
+  formatLamp() {
+    const status = this.props.data ? "on" : "off";
+    const message = status === "on" ? 
+      "Chris Tyler is on campus." : "DB 1036 is dark.";
+ 
+    return(
+      <div className="lamp-container">
+      <img 
+        className="bulb-off" 
+        src={status === "on" ? bulbOn : bulbOff}
+      >
+      </img>
+      <div className="lamp-message">DB 1036 is dark.</div>
+      </div>
+    );
+  }
+
+  formatRoom1042() {
+
+  }
+
   formatRAPresentations() {
 
+  }
+
+  loadSpinner() {
+    return (
+      <div className="spinner-container">
+      <div class="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+      </div>
+    );
   }
 
   render() {
@@ -43,7 +94,7 @@ class Panel extends React.Component {
           </div>
         </div>
         <div className="panel-content">
-          {this.formatOutput(this.props.title)}
+          {this.props.data && this.formatOutput(this.props.title) || this.loadSpinner()}
         </div>
       </div>
     );
