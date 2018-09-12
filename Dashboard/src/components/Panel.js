cb6aa84a65d5716ff15c71b9e36c97be32440f30
belavina <ol.belavina@gmail.com>
2018-09-12 15:54:37 -0400

    Added length checks for EODs

--- Dashboard/src/components/Panel.js
+++ Dashboard/src/components/Panel.js
@@ -107,7 +107,7 @@ class Panel extends React.Component {
 
     return (
       <div>
-        <h3>Today's EODs</h3>
+        {Object.keys(currentEods).length !==0 && <h3>Today's EODs</h3> }
         {Object.keys(currentEods).map((username) => (
           <div className="github-entry">
             <span></span>
@@ -117,7 +117,7 @@ class Panel extends React.Component {
             <p>{currentEods[username].text}</p>
           </div>
         ))}
-        <h3>Past EODs</h3>
+        {Object.keys(oldEods).length !==0 && <h3>Past EODs</h3> }
         {Object.keys(oldEods).map((username) => (
           <div className="github-entry">
             <span></span>
