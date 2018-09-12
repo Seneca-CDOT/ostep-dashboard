7465ae2af50a72d430bbc9139c199126bf7e5795
fahadnac <36707813+fahadnac@users.noreply.github.com>
2018-09-12 14:32:24 -0400

    Add files via upload

--- RAPresenter/index.js
+++ RAPresenter/index.js
@@ -3,10 +3,11 @@ var app = express();
 var scraper = require('table-scraper');
 
 //creates object for sending the rows
-var newObj = {"rows":[]};
+
 
 app.get('/', (req, res)=>{
     //scrapes table data from wiki.cdot website
+    var newObj = {"rows":[]};
     scraper
     .get('https://wiki.cdot.senecacollege.ca/wiki/CDOT_Fall_2018_Weekly_Presentation_Schedule#Presentation_Dates')
     .then(function(tableData){
