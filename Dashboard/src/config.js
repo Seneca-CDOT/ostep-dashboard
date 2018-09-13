b3a84eabcb9eba69f2da11421a5ac6303f7ca9b5
poftadeh <poftadeh2@gmail.com>
2018-09-13 16:44:49 -0400

    Changed db1042 endpoint address

--- Dashboard/src/config.js
+++ Dashboard/src/config.js
@@ -1,6 +1,6 @@
 export default {
   eods: "http://narnia.cdot.systems:8080/eod",
-  db1042: "http://localhost:8084/",
+  db1042: "http://vega.cdot.systems:8084/",
   presentations: "http://vega.cdot.systems:8083",
   lamp: "http://vega.cdot.systems:8080",
 };
