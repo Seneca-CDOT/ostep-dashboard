131bb245e90d3b78ebbdf3ce807540c9de2aca18
poftadeh <poftadeh2@gmail.com>
2018-09-14 11:50:11 -0400

    Add meeting not found message

--- Dashboard/src/components/Panel.js
+++ Dashboard/src/components/Panel.js
@@ -179,19 +179,26 @@ class Panel extends React.Component {
 
   formatDB1042(sortedData) {
     return (
-      sortedData.rows.map((row, i) => (
-        <div key={row + i} className="github-entry meeting">
-          <span className="meeting-time">
-            <img className="meeting-icons" src={clock} alt={"meeting time icon"} />{row["Date and time"].split(' ')[1]}
-          </span>
-          <span className="meeting-topic">
-            <img className="meeting-icons" src={clipboard} alt={"meeting topic icon"} />{row["Purpose"]}
-          </span>
-          <span className="meeting-organizer">
-            <img className="meeting-icons" src={user} alt={"organizer icon"} />{row["Contact person"]}
-          </span>
-        </div>
-      ))
+      <div>
+        {!sortedData.row.length && <div>No Meetings Found for Today.</div>}
+
+        {
+          sortedData.rows.length && sortedData.rows.map((row, i) => (
+            <div key={row + i} className="github-entry meeting">
+              <span className="meeting-time">
+                <img className="meeting-icons" src={clock} alt={"meeting time icon"} />{row["Date and time"].split(' ')[1]}
+              </span>
+              <span className="meeting-topic">
+                <img className="meeting-icons" src={clipboard} alt={"meeting topic icon"} />{row["Purpose"]}
+              </span>
+              <span className="meeting-organizer">
+                <img className="meeting-icons" src={user} alt={"organizer icon"} />{row["Contact person"]}
+              </span>
+            </div>
+          ))
+        }
+
+      </div>
     );
   }
 
