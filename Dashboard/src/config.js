13faeaa6be4001ab7cf122913f9fe155cefc3e34
poftadeh <poftadeh2@gmail.com>
2018-09-18 17:57:24 -0400

    Add localhost to config for testing

--- Dashboard/src/config.js
+++ Dashboard/src/config.js
@@ -1,5 +1,5 @@
 
-const dash_host = process.env.REACT_APP_OSTEP_DASH_HOST;
+const dash_host = process.env.REACT_APP_OSTEP_DASH_HOST || "localhost";
 
 export default {
   eods: `http://${dash_host}:8081/eod`,
@@ -8,12 +8,4 @@ export default {
   db1042: `http://${dash_host}:8084`,
   infrastructure: `http://${dash_host}:8085`,
   github: `http://${dash_host}:8086`
-
-  // Uncomment below for testing locally
-  // eods: `http://localhost:8081/eod`,
-  // lamp: `http://localhost:8082`,
-  // presentations: `http://localhost:8083`,
-  // db1042: `http://localhost:8084`,
-  // infrastructure: `http://localhost:8085`,
-  // github: `http://localhost:8086`
 }
