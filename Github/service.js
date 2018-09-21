a02cf500168c36e6345d8d458503f5faaf1a5771
poftadeh <poftadeh2@gmail.com>
2018-09-21 16:31:06 -0400

    Fix conditional token

--- Github/service.js
+++ Github/service.js
@@ -11,7 +11,7 @@
 
 var config = require("./config.js");
 var request = require("request");
-var key = config.configKey.SECRET_KEY || process.env.GITHUB_TOKEN; 
+var key = process.env.GITHUB_TOKEN || config.configKey.SECRET_KEY; 
 var repoUrl = 'https://api.github.com/orgs/Seneca-CDOT/repos?per_page=100&access_token=' + key;
 console.log(key);
 var repoUrls = [];         // Attributes: 'name', 'url'
