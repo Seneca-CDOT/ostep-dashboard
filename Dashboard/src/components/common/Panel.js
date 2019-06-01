6b24f1f0538d76b2424067170a25c90f6c225790
Miguel Roncancio <mroncanicio19@gmail.com>
2019-06-01 17:12:14 -0400

    change panel from stateless to stateful for toggleDropdown

--- Dashboard/src/components/common/Panel.js
+++ Dashboard/src/components/common/Panel.js
@@ -2,22 +2,36 @@ import React from 'react';
 import refresh from '../assets/refresh.svg';
 import Spinner from './Spinner';
 
-const Panel = (props) => {
-  return (
-    <div className="panel-body">
-      <div className="panel-title">
-        <h3 className="panel-title-text">
-          {props.title}
-        </h3>
-        <div className="refresh-container" onClick={props.refreshData}>
-          <img className="refresh" src={refresh} alt={"refresh icon"} />
+export default class Panel extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      content_visible: true,
+    };
+    this.toggleDropdown = this.toggleDropdown.bind(this);
+  }
+
+  render() {
+    return (
+      <div className="panel-container" >
+        <div className="panel-header">
+          <h3 className="panel-title" onClick={this.toggleDropdown}>
+            {this.props.title}
+          </h3>
+          <div className="refresh-container" onClick={this.props.refreshData}>
+            <img className="refresh" src={refresh} alt={"refresh icon"} />
+          </div>
+        </div>
+        <div className="panel-content" style={{display: this.state.content_visible ? 'block' : 'none'}}>
+          {this.props.children || <Spinner />}
         </div>
       </div>
-      <div className="panel-content">
-        {props.children || <Spinner />}
-      </div>
-    </div>
-  )
-};
+    );
+  }
 
-export default Panel;
+  toggleDropdown() {
+    if (window.innerWidth < 950) {
+      this.setState({ content_visible: !this.state.content_visible });
+    }
+  }
+}
