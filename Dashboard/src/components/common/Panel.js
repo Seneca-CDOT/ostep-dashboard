f554c829cbd67d3a0b31dacc5b8eb70d3c922114
miguel <mroncancio19@gmail.com>
2019-06-05 10:45:43 -0400

    move render to bottom of component, name breakpoint constant, add panel id, add visible/hidden classes

--- Dashboard/src/components/common/Panel.js
+++ Dashboard/src/components/common/Panel.js
@@ -2,50 +2,59 @@ import React from 'react';
 import refresh from '../assets/refresh.svg';
 import Spinner from './Spinner';
 
+const DROPDOWN_BREAKPOINT = 950;
+
 export default class Panel extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      content_visible: true,
-      width: window.innerWidth,
+      contentVisible: true,
+      width: window.innerWidth
     };
     this.toggleDropdown = this.toggleDropdown.bind(this);
   }
 
+  componentDidMount() {
+    window.addEventListener('resize', this.updateDimensions.bind(this));
+  }
+
+  updateDimensions() {
+    this.setState({ width: window.innerWidth });
+  }
+
+  componentDidUpdate() {
+    if (this.state.width >= DROPDOWN_BREAKPOINT && !this.state.contentVisible)
+      this.setState({ contentVisible: !this.state.contentVisible });
+  }
+
+  toggleDropdown() {
+    if (window.innerWidth < DROPDOWN_BREAKPOINT) {
+      this.setState({ contentVisible: !this.state.contentVisible });
+    }
+  }
+
   render() {
     return (
-      <div className="panel-container" >
-        <div className="panel-header">
-          <h3 className="panel-title" onClick={this.toggleDropdown}>
+      <div
+        className='panel-container'
+        id={`${this.props.title.toLowerCase()}-panel`}
+      >
+        <div className='panel-header'>
+          <h3 className='panel-title' onClick={this.toggleDropdown}>
             {this.props.title}
           </h3>
-          <div className="refresh-container" onClick={this.props.refreshData}>
-            <img className="refresh" src={refresh} alt={"refresh icon"} />
+          <div className='refresh-container' onClick={this.props.refreshData}>
+            <img className='refresh' src={refresh} alt={'refresh icon'} />
           </div>
         </div>
-        <div className="panel-content" style={{display: this.state.content_visible ? 'block' : 'none'}}>
+        <div
+          className={`panel-content ${
+            this.state.contentVisible ? 'visible' : 'hidden'
+          }`}
+        >
           {this.props.children || <Spinner />}
         </div>
       </div>
     );
-  }
-
-  componentDidMount() {
-    window.addEventListener("resize", this.updateDimensions.bind(this));
-  }
-
-  updateDimensions() {
-    this.setState({width: window.innerWidth});
-  }
-
-  componentDidUpdate(){
-    if (this.state.width >= 950 && !this.state.content_visible)
-      this.setState({ content_visible: !this.state.content_visible })
-  }
-
-  toggleDropdown() {
-    if (window.innerWidth < 950) {
-      this.setState({ content_visible: !this.state.content_visible });
-    }
   }
 }
