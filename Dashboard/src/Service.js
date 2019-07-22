b9a089ae36814ca3f2bf9af358614a5f30dea479
Pouya Oftadeh <poftadeh2@gmail.com>
2019-07-21 20:15:32 -0400

    fix: change used ports to 80

--- Dashboard/src/Service.js
+++ Dashboard/src/Service.js
@@ -1,33 +1,33 @@
 import http from 'http';
-import config from './config.js';
 import dummyData from './debug/dummy-data';
 
 class Service {
   getData(containerName, cb) {
     if (process.env.REACT_APP_MODE === 'development') {
       cb(dummyData[containerName]);
-      return;
+    } else {
+      http.get(containerName, res => {
+        res.setEncoding('utf8');
+        let body = '';
+
+        res.on('error', function(e) {
+          console.error(e.message);
+        });
+
+        res.on('data', data => {
+          body += data;
+        });
+
+        res.on('end', () => {
+          try {
+            body = JSON.parse(body);
+            cb(body);
+          } catch (e) {
+            console.error(e);
+          }
+        });
+      });
     }
-    const url = config[containerName];
-    http.get(url, res => {
-      res.setEncoding('utf8');
-      let body = '';
-      res.on('error', function(e) {
-        console.error(e.message);
-      });
-      res.on('data', data => {
-        body += data;
-      });
-      res.on('end', () => {
-        try {
-          body = JSON.parse(body);
-          cb(body);
-        } catch (e) {
-          console.log('INVALID JSON');
-          console.log(e);
-        }
-      });
-    });
   }
 }
 
