1904eac110b00606bf0704a6c1bb917765eb1be7
Pouya Oftadeh <poftadeh2@gmail.com>
2019-07-22 00:06:18 -0400

    feat: add conditional data route

--- Dashboard/server.js
+++ Dashboard/server.js
@@ -1,25 +1,31 @@
 const express = require('express');
 const path = require('path');
 const { IpFilter, IpDeniedError } = require('express-ipfilter');
-const whitelist = require('../config-files/whitelist');
+const { isEnabled, ipsAllowed } = require('../config-files/whitelist');
 
 const PORT = process.env.PORT || 80;
 const app = express();
 
-app.use(IpFilter(whitelist, { mode: 'allow' }));
+if (isEnabled) {
+  app.use(IpFilter(ipsAllowed, { mode: 'allow' }));
+}
+
 app.use(express.static(path.join(__dirname, 'build')));
 
+app.get('/data/:containerName', (req, res) => {
+  const { containerName } = req.params;
+  res.send(containerName);
+});
+
 app.use((err, req, res, next) => {
-  console.log('Error handler', err);
+  console.error('Error handler:', err);
   if (err instanceof IpDeniedError) {
     res.status(401);
   } else {
-    res.status(err.status || 500);
+    res.status(err.status || 500).json({ message: err });
   }
-
-  res.send('You shall not pass.');
 });
 
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(`listening on ${PORT}`);
+  console.log(`Dashboard container is listening on port ${PORT}`);
 });
