bd0f277c6ec2b3071c58966e6ab05a8c0c6f8591
Pouya Oftadeh <poftadeh2@gmail.com>
2019-07-24 00:11:25 -0400

    fix: basic auth not executing after denied IP

--- Dashboard/server.js
+++ Dashboard/server.js
@@ -5,7 +5,7 @@ const basicAuth = require('express-basic-auth');
 const { IpFilter, IpDeniedError } = require('express-ipfilter');
 const { whitelist, users } = require('../config-files/authentication');
 
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 8080;
 const app = express();
 
 if (whitelist) {
@@ -26,17 +26,16 @@ app.get('/osteppy/:endpoint', (req, res) => {
   });
 });
 
-app.use((err, _req, res, _next) => {
+app.use((err, _req, res, next) => {
   console.error('Error handler:', err);
   if (err instanceof IpDeniedError) {
-    basicAuth({
-      challenge: true,
-      users,
-    });
+    next();
   } else {
     res.status(err.status || 500).json({ message: err.message });
   }
 });
+
+app.use(basicAuth({ challenge: true, users }));
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Dashboard container is listening on port ${PORT}`);
