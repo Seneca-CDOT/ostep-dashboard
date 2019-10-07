e48fe30323e79ba6ab62c445f148991d986561f2
Josue <josue.quilon-barrios@senecacollege.ca>
2019-10-07 10:48:06 -0400

    Issues/89 help wanted priority regex (#99)
    * Added regexp to help-wanted
    
    * Added regex for priority and used test() for checking if string is present instead of match()

--- Github/service.js
+++ Github/service.js
@@ -34,7 +34,7 @@ module.exports.getRepos = recency => {
         } else {
           const reposNames = JSON.parse(data)
             .filter(rep => {
-              return today - new Date(rep.pushed_at) < recency;
+              return (today - new Date(rep.pushed_at) < recency && !rep.private);
             })
             .map(repo => {
               return {
@@ -161,7 +161,6 @@ const getIssuesFromRepo = repo => {
     request.get(
       {
         url: `/repos/Seneca-CDOT/${repo.name}/issues`,
-        qs: { labels: 'help wanted' },
       },
       (err, res, data) => {
         if (res.statusCode !== 200) {
@@ -170,39 +169,48 @@ const getIssuesFromRepo = repo => {
         } else {
           const filteredIssues = JSON.parse(data).map(issue => {
             let assigs = [];
-            if (issue.assignees.length !== 0) {
-              assigs = issue.assignees.map(assignee => {
-                return {
-                  name: assignee.login,
-                  avatar: assignee.avatar_url,
-                };
-              });
-            }
-            const labelsInIssue = issue.labels.map(label => label.name);
 
-            return {
-              ra: issue.user.login,
-              repository: issue.repository_url.slice(
-                issue.repository_url.lastIndexOf('/') + 1,
-                issue.repository_url.length,
-              ),
-              number: issue.number,
-              title: issue.title,
-              description: issue.body,
-              language: repo.language,
-              labels: labelsInIssue,
-              state: issue.state,
-              assignees: assigs,
-              milestone: issue.milestone,
-              created: new Date(issue.created_at).toLocaleString('en-US', {
-                timeZone: 'America/Toronto',
-              }),
-              updated: new Date(issue.updated_at).toLocaleString('en-US', {
-                timeZone: 'America/Toronto',
-              }),
-            };
+            helpWantedRegExp = /help( |-)?wanted/i;
+            hasHelpWanted = (label) => {
+              return helpWantedRegExp.test(label.name);
+            }
+
+            if (issue.labels.some(hasHelpWanted)) {
+              if (issue.assignees.length !== 0) {
+                assigs = issue.assignees.map(assignee => {
+                  return {
+                    name: assignee.login,
+                    avatar: assignee.avatar_url,
+                  };
+                });
+              }
+              const labelsInIssue = issue.labels.map(label => label.name);
+  
+              return {
+                ra: issue.user.login,
+                repository: issue.repository_url.slice(
+                  issue.repository_url.lastIndexOf('/') + 1,
+                  issue.repository_url.length,
+                ),
+                number: issue.number,
+                title: issue.title,
+                description: issue.body,
+                language: repo.language,
+                labels: labelsInIssue,
+                state: issue.state,
+                assignees: assigs,
+                milestone: issue.milestone,
+                created: new Date(issue.created_at).toLocaleString('en-US', {
+                  timeZone: 'America/Toronto',
+                }),
+                updated: new Date(issue.updated_at).toLocaleString('en-US', {
+                  timeZone: 'America/Toronto',
+                }),
+              };
+  
+            }
           });
-          resolve(filteredIssues);
+          resolve(filteredIssues.filter(issue => (issue !== undefined && issue !== null)));
         }
       },
     );
@@ -272,6 +280,14 @@ const getPullRequestsPerRepo = repo => {
               }),
               number: pullRequest.number,
             };
+            priorityRegEx = /priority/i;
+            hasPriority = (label) => {
+              return priorityRegEx.test(label.name);
+            }
+
+            if (!pullRequestData.labels.some(hasPriority))
+              pullRequestData.labels.push({name: "priority: medium", color: "e4fc5a"})
+
             pullRequestsPerBranch.push(pullRequestData);
           });
           resolve(pullRequestsPerBranch);
