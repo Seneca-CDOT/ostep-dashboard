2ada32a58770dccea802ef68da8e6788355259e9
Pouya Oftadeh <poftadeh2@gmail.com>
2019-10-21 23:36:30 -0400

    feat: add moment duration function

--- Dashboard/src/components/Reminder.js
+++ Dashboard/src/components/Reminder.js
@@ -15,79 +15,62 @@ export default class Reminder extends Container {
     super(props, COMPONENT_NAME);
   }
 
+  computeDurations = ({created}) => {
+    const now = moment();
+    const createdDate = moment(created);
+    const timeDifference = moment.duration(now.diff(createdDate));
+    return {
+      days: timeDifference.days(),
+      hours: timeDifference.hours(),
+      minutes: timeDifference.minutes(),
+      seconds: timeDifference.seconds(),
+    };
+  };
+
   render() {
     return (
       <Panel title="PR Reminder" refreshData={this.refreshData}>
         {this.state.data &&
           this.state.data.map(pullRequest => {
-            const creationDate = moment(pullRequest.created).format(
-              'MMM DD@HH:mm'
-            );
+            const { days, hours, minutes, seconds } = this.computeDurations(pullRequest);
             return (
               <div className="github-pullRequest">
-                <img
-                  className="github-pullRequest__icon"
-                  src={pullRequestIcon}
-                  alt="Pull Request icon"
-                />
                 <div
                   key={`${pullRequest.repoName}`}
                   className="github-pullRequest__content"
                 >
                   <div className="github-pullRequest__details">
+                    <img
+                      className="github-pullRequest__icon"
+                      src={pullRequestIcon}
+                      alt="Pull Request icon"
+                    />
                     <span className="github-pullRequest__repo github-pullRequest__label">
                       {`[${pullRequest.repoName}]`}
                     </span>
                     <span className="github-pullRequest__priority--high github-pullRequest__label">
-                      {'[Priority: High]'}
+                      {'[High]'}
                     </span>
-                    <span className="github-pullRequest__name">
-                      {pullRequest.title}
-                    </span>
-                    <span className="github-pullRequest__time">
-                      {' ('}
-                      {creationDate}
-                      {') '}
+                    <span className="github-pullRequest__name github-pullRequest__label">
+                      {`#${pullRequest.number}: ${pullRequest.title}`}
                     </span>
                   </div>
                   <div className="github-pullRequest__status">
-                    <span className="github-pullRequest__time">5</span>days old
-                    - Waiting on{' '}
-                    <span>
-                      {pullRequest.reviewers.map(reviewer => (
+                    <span className="github-pullRequest__time">
+                      {days}
+                    </span>
+                    days old - waiting on{' '}
+                    {pullRequest.reviewers.map(reviewer => (
+                      <span className="github-pullRequest__reviewer">
                         <img
                           className="github-pullRequest__avatar"
                           src={reviewer.avatar}
                           title={reviewer.name}
+                          alt={`${reviewer.name}'s avatar'`}
                         />
-                      ))}
-                    </span>
-                    {/* {pullRequest.labels.map(label => {
-                    const text = this.textColor(label.color);
-                    const inLineStyle = {
-                      background: `#${label.color}`,
-                      color: `${text}`
-                    };
-                    return (
-                      <span
-                        style={inLineStyle}
-                        className="github-pullRequest__label"
-                      >
-                        {`${label.name}`}
                       </span>
-                    );
-                  })} */}
+                    ))}
                   </div>
-                  {/* <div>
-                    <span
-                      className="github-pullRequest__description"
-                      title={pullRequest.description}
-                    >
-                      {' "'}
-                      {`${pullRequest.description}`}
-                      {' "'}
-                    </span>
-                  </div> */}
                 </div>
               </div>
             );
@@ -95,20 +78,4 @@ export default class Reminder extends Container {
       </Panel>
     );
   }
-
-  /*
-    textColour uses this to determine if the label font should be white or black
-    depending on the background colour
-    https://stackoverflow.com/questions/3942878/how-to-decide-font-color-in-white-or-black-depending-on-background-color/3943023
-  */
-  textColor = colour => {
-    const white = '#ffffff';
-    const black = '#000000';
-
-    const red = parseInt(colour.substr(0, 2), 16);
-    const green = parseInt(colour.substr(2, 2), 16);
-    const blue = parseInt(colour.substr(4, 2), 16);
-
-    return red * 0.299 + green * 0.587 + blue * 0.114 > 186 ? black : white;
-  };
 }
